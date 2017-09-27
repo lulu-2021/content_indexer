549f9c58a9686d844d1ce933c87ac414d6017638
Steve Forkin <steve.forkin@gmail.com>
2017-09-27 12:59:26 +1000

    adding some more documentation

--- lib/index/index.ex
+++ lib/index/index.ex
@@ -1,12 +1,16 @@
 defmodule ContentIndexer.Index do
   @moduledoc """
     struct to store the details of what data is held in the index
+    It provides a `new/2` function for instantiating the struct that includes a generated UUID
   """
   alias Ecto.UUID
   alias ContentIndexer.Index
 
   defstruct uuid: "", file_name: "", tokens: [], term_weights: %{}
 
+  @doc """
+
+  """
   def new(file_name, tokens) when is_list(tokens) do
     new_uuid = UUID.generate()
     %Index{file_name: file_name, tokens: tokens, uuid: new_uuid}
